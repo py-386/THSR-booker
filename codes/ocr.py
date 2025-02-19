253aa3cb16b1fbdd69502727502dcc36424236fc
py-386 <billy121458596@gmail.com>
2025-02-19 23:58:38 +0800

    first push

---
+++ codes/ocr.py
@@ -0,0 +1,13 @@
+import ddddocr
+
+
+
+ocr = ddddocr.DdddOcr()
+
+
+
+def get_captcha_code():
+    image = open('chptcha.png', "rb").read() # 'rb'是以二進位讀取進來
+    result = ocr.classification(image)
+    print("Get captcha code : ", result)
+    return result
---
+++ codes/selenium_thsr_booker.py
@@ -0,0 +1,62 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select #下拉式選單使用
+from ocr import get_captcha_code
+
+driver = webdriver.Chrome() 
+
+driver.get('https://irs.thsrc.com.tw/IMINT/')
+
+driver.implicitly_wait(2)
+
+acc_button = driver.find_element(by=By.ID, value='cookieAccpetBtn')
+time.sleep(1)
+acc_button.click() # 按按鈕
+
+#下拉式選單
+#出發站
+start_dropdown = driver.find_element(By.XPATH, "//select[@class='uk-select' and @name='selectStartStation']")  #找下拉選單
+start_select = Select(start_dropdown)
+start_select.select_by_visible_text('台中')
+
+#抵達站
+destination_dropdown = driver.find_element(By.XPATH, "//select[@class='uk-select' and @name='selectDestinationStation']")
+destination_select = Select(destination_dropdown)
+destination_select.select_by_visible_text('台北')
+
+#出發時間
+to_time_dropdown = driver.find_element(By.NAME, 'toTimeTable')
+to_time_select = Select(to_time_dropdown)
+to_time_select.select_by_visible_text('06:30')
+
+#出發日期
+driver.find_element(By.XPATH, "//input[@class='uk-input' and @readonly='readonly']").click()
+time.sleep(1)
+
+start_date = "二月 21, 2025"
+driver.find_element(By.XPATH, f"//span[@class='flatpickr-day' and @aria-label='{start_date}']").click()
+
+#驗證碼-圖
+captcha_img = driver.find_element(by=By.ID, value='BookingS1Form_homeCaptcha_passCode')
+captcha_img.screenshot('chptcha.png') #截圖存成檔案
+
+#ocr 辨識
+captcha_code = get_captcha_code()
+
+#填入驗證碼
+
+captcha_input = driver.find_element(By.ID, 'securityCode')
+captcha_input.send_keys(captcha_code) #輸入驗證碼
+
+
+#開始查詢
+
+driver.find_element(By.ID, 'SubmitButton').click()
+
+
+time.sleep(5)
+driver.quit()
+
+
+
